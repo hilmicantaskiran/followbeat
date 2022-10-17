535269dbbf68fc244f882a792ffab853c080c812
Hilmi Can Taşkıran <hilmicantaskiran@gmail.com>
2022-10-17 16:20:22 +0300

    Update Home page and create components

---
+++ components/index.js
@@ -0,0 +1,7 @@
+export { default as Footer } from './footer';
+export { default as Layout } from './layout';
+export { default as Product } from './product';
+export { default as Navbar } from './navbar';
+export { default as FooterBanner } from './FooterBanner';
+export { default as HeroBanner } from './HeroBanner';
+export { default as Cart } from './Cart';
