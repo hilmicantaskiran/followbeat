3a05413da7b5597d566b25b4e7683bdfae2dcb89
Hilmi Can Taşkıran <hilmicantaskiran@gmail.com>
2022-10-17 22:02:36 +0300

    Create layout, navbar, footer components

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
+
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      <p>2022 FollowBeat All rights reserved</p>
+      <p className='icons'>
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
   )
 }
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai';
+
+
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href="/">FollowBeat</Link>
+      </p>
+      <button
+        type='button'
+        className='cart-icon'
+        onClick=""
+      >
+        <AiOutlineShopping />
+        <span className="cart-item-qty">1</span>
+      </button>
+    </div>
   )
 }
 
-export default Navbar
+export default Navbar;
